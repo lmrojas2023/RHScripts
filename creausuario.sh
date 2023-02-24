842a7a7ec61a1178f604a4abd4c3699c4f63cf9e
lmrojas2023 <lmr_rojas@cursoredhat.fhlomanb5gre3dx4ykxytrcbee.bx.internal.cloudapp.net>
2023-02-24 08:57:27 +0000

    commit

--- creausuario.sh
+++ creausuario.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-sudo useradd -m -p LaLiga-rules29 malvin
-sudo cho 'Welcome to W Corp' | cat > /home/malvin/welcome.txt
+sudo useradd -p LaLiga-rules29 malvin
+sudo mkdir /home/malvin
+sudo chmod 777 /home/malvin
+sudo echo 'Welcome to W Corp' | cat > /home/malvin/welcome.txt
 sudo chmod 644 /home/malvin/welcome.txt
+sudo chown malvin:malvin -R /home/malvin
+sudo chmod 755 /home/malvin
+sudo shown malvin:malvin -R /home/malvin
+sudo usermod -d /home/malvin
+
 
